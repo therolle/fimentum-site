2faba2bb1d54fe72d5abd710d8a36cd4e76e6960
Rolf Koski <rolle@iki.fi>
2025-12-10 14:55:48 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 8801,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
